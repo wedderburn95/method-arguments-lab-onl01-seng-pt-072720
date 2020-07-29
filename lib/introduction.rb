74580ebf0b5f12373fcdbc36bed531092e27e5db
dauntless-panel-9117 <wedderburn95@yahoo.com>
2020-07-29 06:13:33 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,5 +1,7 @@
 def introduction(name)
-  puts "Hi, my name is #{name, Spanish}."
+  puts "Hi, my name is #{name}."
 end
 
-def 
+def introduction_with_language(name,language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
